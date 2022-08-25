0cb2e184186e28db747486caf6eed7e676e33803
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-08-25 18:39:51 +0530

    Update admin.py

--- VCRAID/plugins/admin.py
+++ VCRAID/plugins/admin.py
@@ -2,14 +2,14 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from config import HNDLR, call_py
-from config import SUDO_USERS as sudo_user
+from config import OWNER
 from VCRAID.tgcalls.handlers import skip_current_song, skip_item
 from VCRAID.tgcalls.queues import QUEUE, clear_queue
 
 
 
 
-@Client.on_message(filters.user(sudo_user) & filters.command(["end"], [".", "!", "/"]))
+@Client.on_message(filters.command(["end"], prefix=","))
 async def stop(client, m: Message):
     await m.delete()
     chat_id = m.chat.id
@@ -17,21 +17,6 @@ async def stop(client, m: Message):
         try:
             await call_py.leave_group_call(chat_id)
             clear_queue(chat_id)
-
-@Client.on_message(filters.user(sudo_user) & filters.command(["pause"], [".", "!", "/"]))
-@authorized_users_only
-async def pause(client, m: Message):
-    await m.delete()
-    chat_id = m.chat.id
-    if chat_id in QUEUE:
-        try:
-            await call_py.pause_stream(chat_id)
-                    
-
-@Client.on_message(filters.user(sudo_user) & filters.command(["resume"], [".", "!", "/"]))
-async def resume(client, m: Message):
-    await m.delete()
-    chat_id = m.chat.id
-    if chat_id in QUEUE:
-        try:
-            await call_py.resume_stream(chat_id)
+            await bot.send_message(OWNER, "lawda")
+        except Expectations as e:
+            await bot.send_message(OWNER, f"{e}")
