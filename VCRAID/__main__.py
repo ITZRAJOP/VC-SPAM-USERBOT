b04881a4e58385b550454901f3353df97052a41f
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-08-25 16:41:45 +0530

    Done setup main.py

--- VCRAID/__main__.py
+++ VCRAID/__main__.py
@@ -1,1 +1,18 @@
+import asyncio
+from pytgcalls import idle
+from VCRAID import call_py
 
+async def main():
+    await call_py.start()
+    await bot.send_message(SUPPORT, "**VCRAID UserBot Successfully Deployed And Started**")
+    print(
+        """
+    ------------------
+   | Userbot Started! POWERED BY @THEDEADLYBOTS |
+    ------------------
+"""
+    )
+    await idle()   
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
