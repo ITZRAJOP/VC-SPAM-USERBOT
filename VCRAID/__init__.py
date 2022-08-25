cd5425de0d446063a2796de1eb248d797efda6a7
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-08-25 16:43:45 +0530

    Update __init__.py

--- VCRAID/__init__.py
+++ VCRAID/__init__.py
@@ -1,4 +1,6 @@
 from config import *
+from pytgcalls import PyTgCalls
+from pyrogram import Client, filters
 
 API_ID = API_ID
 API_HASH = API_HASH
@@ -8,3 +10,11 @@ LOG_GROUP = LOG_GROUP
 
 DEADLY_VERSION = "0.0.1"
 
+
+contact_filter = filters.create(
+    lambda _, __, message: (message.from_user and message.from_user.is_contact)
+    or message.outgoing
+)
+
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="VCRAID.plugins"))
+call_py = PyTgCalls(bot)
