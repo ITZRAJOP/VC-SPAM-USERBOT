f67c95de0cee18c3f213bea0036261cb517436bc
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-08-25 17:55:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION", "session")
-OWNER = os.getenv("OWNER", "")
+API_ID = int(os.getenv("API_ID", "3898418"))
+API_HASH = os.getenv("API_HASH", "5a82313211da04d63297bd4de436228c")
+SESSION = os.getenv("SESSION", "")
+OWNER = os.getenv("OWNER", "Elric_xD")
 SUPPORT = os.getenv("SUPPORT", "TheDeadlyBots")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
